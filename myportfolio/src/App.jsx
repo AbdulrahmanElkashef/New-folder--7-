dcd167b09b09b41182aced2ea4b940a1d508737d
abdulrahman elkashef <abdulrahmanelkashef@gmail.com>
2024-05-13 05:09:20 +0300

    first commit

---
+++ myportfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import Header from './components/1-Header/header';
+import Hero from './components/2-Hero/hero';
+import Main from './components/3-Main/main';
+import Contact from './components/4-Contact/contact';
+import Footer from './components/footer/footer';
+
+
+function App() {
+
+  return (
+    <> 
+    <div className='container'>
+
+      <Header/>
+      <div className='divider' />
+      <Hero />
+      <div className='divider' />
+      <Main />
+      <div className='divider' />
+      <Contact />
+      <div className='divider' />
+      <Footer />
+
+    </div>
+    </>
+  )
+}
+
+export default App; 
---
+++ myportfolio/src/components/1-Header/header.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './header.css';
+
+ function Header() {
+  const [showModel, setshowModel] = useState(false);
+  return (
+    <header className='flex'>
+      <button onClick={() => {setshowModel(true) }} className='menu'>show model</button>
+
+      <div />
+      <nav>
+        <ul className='flex'>
+          <li><a href='#About'>About</a></li>
+          <li><a href='#Articles'>Articles</a></li>
+          <li><a href='#Project'>Project</a></li>
+          <li><a href='#Speaking'>Speaking</a></li>
+          <li><a href='#Contact'>Contact</a></li>
+        </ul>
+      </nav>
+
+      <button>light</button>
+   {showModel && (
+    <div className='fixed'>
+       
+       <ul className='model'>
+         <li><a href='#About'>About</a></li>
+         <li><a href='#Articles'>Articles</a></li>
+         <li><a href='#Project'>Project</a></li>
+         <li><a href='#Speaking'>Speaking</a></li>
+         <li><a href='#Uses'>Uses</a></li>
+       </ul>
+       </div>
+   )}
+
+    </header>
+  )
+}
+
+export default Header;
