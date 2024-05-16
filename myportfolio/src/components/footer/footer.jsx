6e1354f6aa9b72be44a442f3b68a96df33c860be
abdulrahman elkashef <abdulrahmanelkashef@gmail.com>
2024-05-17 02:59:40 +0300

    finish footer section

--- myportfolio/src/components/footer/footer.jsx
+++ myportfolio/src/components/footer/footer.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './footer.css'
- function Footer() {
+import "./footer.css";
+function Footer() {
   return (
-    <footer>
-      footer
+    <footer className="flex">
+
+      <ul className="flex">
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Project">Project</a>
+        </li>
+        <li>
+          <a href="#Speaking">Speaking</a>
+        </li>
+        <li>
+          <a href="#Uses">Uses</a>
+        </li>
+      </ul>
+    
+      <p>©2024 Spencar Sharp. All rights reserved.</p>
     </footer>
-  )
+  );
 }
 export default Footer;
