e4590745ff8241aa2853d5895a1cfd281c8b12f3
abdulrahman elkashef <abdulrahmanelkashef@gmail.com>
2024-05-16 04:23:43 +0300

    finish content section

--- myportfolio/src/components/4-Contact/contact.jsx
+++ myportfolio/src/components/4-Contact/contact.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import './contact.css'
- function Contact() {
+import React from "react";
+import "./contact.css";
+function Contact() {
   return (
-    <div>
-      contact
-    </div>
-  )
+    <section className="content-us">
+      <h1 className="title">
+        <span className="icon-envelope"></span>
+        Contact us
+      </h1>
+      <p>
+        Contact us for more information and Get notified when I publish
+        something new.
+      </p>
+
+      <div className="flex ">
+        <form>
+          <div className="flex">
+            <label htmlFor="email">Email Address:</label>
+            <input required type="email" name="" id="email"></input>
+          </div>
+
+
+          <div className="flex" style={{marginTop: "24px"}}>
+            <label htmlFor="message">Your Message:</label>
+            <textarea required name="" id="message"></textarea>
+          </div>
+
+          <button className="submit">Submit</button>
+        </form>
+      </div>
+
+      <div className="animation"></div>
+    </section>
+  );
 }
 export default Contact;
